0a1129c202a4b027db03fe701d031a3856877a7b
Cristian Schütz <cristianschutz@hotmail.com>
2017-05-08 09:26:25 -0300

    ajustes página speedtest

--- portal-copel-telecom/page-speedtest.php
+++ portal-copel-telecom/page-speedtest.php
@@ -16,75 +16,151 @@
 				</article>
 			</div>
 		<?php else: ?>
+
 			<header class="wow fadeIn">
 				<h1 class="main-tt main-tt-barra">Speedtest Copel Telecom</h1>
-				<small>Teste agora a qualidade da sua internet banda larga com o teste de velocidade da Copel Telecom.</small>
 			</header>
+
+			<span class="space-20"></span>
+
 			<div id="speedtest-valid">
+
 				<article id="speedtest-teste">
 					<div class="container">
 						<div class="row">
-							<aside data-mh="teste-group" class="col-sm-4 col-xs-12">
-								<h2>Você sabia?</h2>
-								<p>Que a Copel Fibra é a única internet no Brasil com tecnologia 100% em fibra óptica, a rede de transmissão de dados mais veloz que existe em todo o mundo?</p>
-								<p>Copel Fibra é a internet da Copel Telecom para todos os paranaenses.</p>
-								<a href="<?php $p = get_page_by_title("Copel Telecom"); echo get_permalink($p->ID); ?>" class="bt-o-b bt-o-b-white">SAIBA MAIS SOBRE A COPEL FIBRA </a>
-							</aside>
-							<div class="col-sm-8 col-xs-12" data-mh="teste-group">
-								<iframe src="http://speedtest.copel.net/speedtest_geral_new.html" frameborder="0" height="100%" width="100%"></iframe>
+							
+							<div class="col-md-8 col-sm-12 col-xs-12 wow fadeIn">
+								
+								<iframe src="http://speedtest.copel.net/speedtest_geral_new.html" frameborder="0" height="465" width="100%"></iframe>
+								
+								<aside>
+
+									<h2>Veja como a Copel Telecom garante o melhor resultado</h2>
+									<p>A Copel Telecom garante a entrega da velocidade contratada, sem diferenças, tanto download como para upload. E isso, só quem tem a rede de transmissão de dados mais veloz do mundo pode oferecer.</p>
+
+									<ul>
+										
+										<li>
+											<img src="<?php bloginfo('template_url'); ?>/_assets/images/speedtest/i-fibra.png" alt="">
+											Fibra Óptica<br><strong>de ponta a ponta</strong>
+										</li>
+
+										<li>
+											<img src="<?php bloginfo('template_url'); ?>/_assets/images/speedtest/i-time.png" alt="">
+											Velocidades de<br><strong>30 a 150 mega</strong>
+										</li>
+
+										<li>
+											<img src="<?php bloginfo('template_url'); ?>/_assets/images/speedtest/i-upload.png" alt="">
+											<strong>O maior upload</strong><br>do mercado
+										</li>
+
+									</ul>
+
+
+								</aside>
+
 							</div>
+
+							<div class="col-md-4 col-sm-12 col-xs-12 speedtest-dicas wow fadeIn">
+								
+								<h6>Dicas importantes para realizar o teste</h6>
+								
+								<p>O teste de velocidade da Copel Telecom é um dos mais utilizados do Brasil. É um serviço online oferecido pela Copel Telecom com base no velocímetro Ookla. O objetivo é detectar com precisão a velocidade de transferência de dados, informando se a banda atual está de acordo com a velocidade contratada.</p>
+								<p><strong>Antes de medir a velocidade da sua banda, siga estas dicas para que o resultado seja o mais preciso possível:</strong></p>
+
+								<span class="space-20"></span>
+
+								<ul>
+
+									<li>
+										
+										<div class="inner-dica">
+										
+											<i class="i-st-cabo"></i> 
+											Faça o teste em um computador conectado à internet por meio de um cabo de rede. <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="bottom" title="Evite redes sem fio. Testes realizados por meio de redes sem fio sofrem a influência de fatores como a criptografia dos dados trafegados, limites (throughput) do access point, interferências eletromagnéticas, entre outros. Se o seu computador possui entrada para cabo de rede, utilize-o para um resultado mais preciso."></span>
+										
+										</div>
+										
+									</li>
+
+									<li>
+
+										<div class="inner-dica">
+																					
+											<i class="i-st-fibra"></i> 
+											Conecte diretamente na entrada do provedor
+
+										</div>
+										
+									</li>
+
+									<li>
+										
+										<div class="inner-dica">
+
+											<i class="i-st-feche"></i> 
+											Feche todos os programas
+
+										</div>
+										
+									</li>
+
+									<li>
+										
+										<div class="inner-dica">
+										
+											<i class="i-st-pare"></i> 
+											Interrompa as atualizações
+
+										</div>
+										
+									</li>
+
+									<li>
+										
+										<div class="inner-dica">
+											
+											<i class="i-st-repita"></i> 
+											Repita o teste
+
+										</div>
+										
+									</li>
+
+								</ul>
+
+							</div>
+	
 						</div>
-					</div>
-				</article>
-			</div>
-			<div class="modal fade" tabindex="-1" id="modalAviso" role="dialog">
-				<div class="modal-dialog modal-dialog-g">
-					<div class="modal-content">
-						<button type="button" data-dismiss="modal" class="i-c-modal-close"></button>
-						<div class="modal-body">
-							<h6>Dicas importantes para realizar o teste</h6>
-							<div class="little">
-								<p>
-									O teste de velocidade da Copel Telecom que mede a largura de banda é um dos mais utilizados do Brasil. O teste é um serviço online oferecido pela Copel Telecom com base no velocímetro Ookla. O objetivo é detectar com precisão a velocidade de transferência de dados, informando se a banda atual está de acordo com a velocidade contratada.<br><br>
-									<strong>Antes de medir a velocidade da sua banda, siga estas dicas para que o resultado seja o mais preciso possível:</strong>
-								</p>
-							</div>
-							<ul class="row list-dicas">
-								<li class="col-md-15 col-sm-6 col-xs-12">
-									<i class="i-st-cabo"></i><br>
-									Faça o teste em um computador concectado a internet por meio de um cabo de rede <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="bottom" title="Evite redes sem fio. Testes realizados por meio de redes sem fio sofrem a influência de fatores como a criptografia dos dados trafegados, limites (throughput) do access point, interferências eletromagnéticas, entre outros. Se o seu computador possui entrada para cabo de rede, utilize-o para um resultado mais preciso."></span>
-								</li>
-								<li class="col-md-15 col-sm-6 col-xs-12">
-									<i class="i-st-fibra"></i><br>
-									Conecte diretamente na<br>entrada do provedor <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="bottom" title=""></span>
-								</li>
-								<li class="col-md-15 col-sm-6 col-xs-12">
-									<i class="i-st-feche"></i><br>
-									Feche todos<br>os programas <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="bottom" title=""></span>
-								</li>
-								<li class="col-md-15 col-sm-6 col-xs-12">
-									<i class="i-st-pare"></i><br>
-									Interrompa as<br>atualizações <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="bottom" title=""></span>
-								</li>
-								<li class="col-md-15 col-sm-12 col-xs-12">
-									<i class="i-st-repita"></i><br>
-									Repita o teste <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="bottom" title=""></span>
-								</li>
-							</ul>
-							<div class="text-center row modal-bts">
-								<div class="col-sm-10 col-sm-offset-1 col-xs-12">
-									<div class="col-md-4 col-md-offset-2 col-sm-6 col-xs-12">
-										<a href="<?php $p = get_page_by_title("Copel Telecom"); echo get_permalink($p->ID); ?>" class="bt-o">saiba mais sobre copel fibra</a>
-									</div>
-									<div class="col-md-4 col-sm-6 col-xs-12">
-										<button type="button" class="bt-o" data-dismiss="modal">continue navegando</button>
-									</div>
+
+						<footer>
+
+							<div class="row text-center wow fadeIn">
+								
+								<div class="space-45"></div>
+
+								<div class="col-xs-12">
+
+									<a href="<?php $p = get_page_by_title("Planos"); echo get_permalink($p->ID); ?>" class="bt-o-b bt-o-b-white mrg5R mrg5B">VEJA OS PLANOS E PREÇOS</a>
+									
+									<a href="http://www.copel.com/wvtweb/site/verificar_disponibilidade.jsf?_ga=1.236576769.108334761.1491312522#" target="_blank" class="bt-o-b bt-o-b-white mrg5L mrg5B">CONTRATAR</a>
+									
 								</div>
+
+								<div class="space-40"></div>
+
 							</div>
-						</div>
+
+						</footer>
+
 					</div>
-				</div>
+
+				</article>
+
 			</div>
+
+	
 			<div class="modal fade" tabindex="-1" id="modalGostou" role="dialog">
 				<div class="modal-dialog">
 					<div class="modal-content">
@@ -111,11 +187,7 @@
 					</div>
 				</div>
 			</div>
-			<script>
-				jQuery(document).ready(function($) {
-					$('#modalAviso').modal('show');
-				});
-			</script>
+
 		<?php endif; ?>
 		<aside>
 			<?php get_template_part('template','bts-contato'); ?>
