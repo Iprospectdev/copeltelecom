d15bf19efaddc0c5628fca4242397f81aa0e9485
Cristian Schütz <cristianschutz@hotmail.com>
2017-07-14 15:59:24 -0300

    esporte interativo ajustes

--- portal-copel-telecom/page-esporte-interativo.php
+++ portal-copel-telecom/page-esporte-interativo.php
@@ -10,7 +10,7 @@
 				<div class="row">
 					<div class="col-md-6 col-sm-9 col-xs-12">
 						<h1>Cliente Copel Telecom, tem acesso gratuito ao conteúdo do Esporte Interativo</h1>
-						<p>Com ele, você pode acompanhar as maiores competições esportivas do planeta! Aproveite, o beneficio vai até o final de maio. Não perca tempo!</p>
+						<p>Com ele, você pode acompanhar as maiores competições esportivas do planeta! Não perca tempo!</p>
 						<a href="#sobre" class="bt bt-orange">SAIBA COMO ASSISTIR</a>
 						<small>*Disponível para os planos residenciais a partir de 30 Mbps.</small>
 						<div class="clearfix"></div>
@@ -41,7 +41,7 @@
 						<span class="space-50"></span>
 						<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/logo.jpg" class="logo-ei" alt="Canais Esporte Interativo">
 						<h3>A paixão e a emoção pelo esporte, onde você estiver</h3>
-						<p>Assista aos principais conteúdos oferecidos pelos Canais Esporte Interativo e fique por dentro de tudo o que acontece no mundo dos esportes. São programas imperdíveis, como a <strong class="text-orange">UEFA Champions League</strong> (maior competição de clubes do mundo) e a <strong class="text-orange">E-League</strong> (maior competição de Counter Strike), além da <strong class="text-orange">National Football League (NFL)</strong>, a maior liga de futebol americano que conecta você às novidades dos times dos Estados Unidos.</p>
+						<p>Assista aos principais conteúdos oferecidos pelos Canais Esporte Interativo e fique por dentro de tudo o que acontece no mundo dos esportes. São programas imperdíveis, como a <strong class="text-orange">UEFA Champions League</strong> (maior competição de clubes do mundo) e a <strong class="text-orange">E-League</strong> (maior competição de Counter Strike).</p>
 						<p>Você também pode ficar ligado nas transmissões das lutas mais surpreendentes de <strong class="text-orange">MMA</strong> e em toda a emoção do <strong class="text-orange">Pro Wrestling.</strong> É hora de desfrutar por completo do mundo dos esportes através de toda a programação dos Canais do Esporte Interativo.</p>
 
 					</div>
@@ -64,8 +64,11 @@
 						<div class="col-sm-4">
 
 							<h3>Sobre o Esporte Interativo</h3>
-							<p>O Esporte Interativo é uma das maiores marcas do mundo dedicada à produção e distribuição de conteúdos esportivos em múltiplas plataformas. Formado pelos canais <span class="text-orange">Esporte Interativo</span> e <span class="text-orange">Esporte Interativo 2</span>, o EI Plus, tem o objetivo de estar próximo do público apaixonado por esportes, onde ele estiver. </p>
-							<p><strong>No EI Plus você pode acompanhar todos os conteúdos dos Canais Esporte interativo no celular, no computador, na smart TV LG, no tablet ou na Apple TV.</strong></p>
+
+							<p>O Esporte Interativo é uma das maiores marcas do mundo dedicada à produção e distribuição de conteúdos esportivos em múltiplas plataformas. Reunindo todo o conteúdo do canal, o Esporte Interativo Plus tem o objetivo de estar próximo do público apaixonado por esportes, levando o melhor da temporada, onde ele estiver.</p>
+
+							<p>No aplicativo Esporte Interativo Plus você pode acompanhar todos os conteúdos dos Canais Esporte Interativo no celular, no computador, na smart TV LG, no tablet ou na Apple TV.</p>
+
 							<a href="#como-acessar" class="bt bt-orange">SAIBA COMO ACESSAR</a>
 						</div>
 
@@ -98,7 +101,12 @@
 						<h3>Clientes Copel Telecom</h3>
 
 						<div class="col-sm-10 col-sm-offset-1 text-center">
-							<p>Confira abaixo como é fácil baixar o aplicativo do <strong class="text-orange">EI Plus</strong> na seu smartphone ou tablet:<br>*O plano do <strong class="text-orange">EI Plus</strong> permite até dois acessos simultâneos.</p>
+
+							<p>
+								Confira abaixo como é fácil baixar o aplicativo do <strong class="text-orange">Esporte Interativo Plus</strong> na seu smartphone ou tablet:<br>
+								*O plano do <strong class="text-orange">Esporte Interativo Plus</strong> permite até dois acessos simultâneos.
+							</p>
+
 						</div>
 
 					</div>
@@ -116,7 +124,7 @@
 						<li class="col-sm-3 col-xs-6 mrg30B">
 							<div data-mh="group-li">
 								<i>2</i>
-								<span>Acesse a loja de aplicativos no seu Smartphone ou tablet e baixe o aplicativo <strong>EI Plus</strong></span>
+								<span>Acesse a loja de aplicativos no seu Smartphone ou tablet e baixe o aplicativo <strong>Esporte Interativo Plus</strong></span>
 								<span class="space-20"></span>
 								<a href="https://itunes.apple.com/us/app/esporte-interativo-ao-vivo/id509496846?ls=1&mt=8" target="_blank">
 									<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-store.png" alt="App Store">
@@ -130,7 +138,7 @@
 						<li class="col-sm-3 col-xs-6 mrg30B">
 							<div data-mh="group-li">
 								<i>3</i>
-								<span>No aplicativo <strong>EI Plus</strong>, faça o login com o dados de acesso recebidos por e-mail*</span>
+								<span>No aplicativo <strong>Esporte Interativo Plus</strong>, faça o login com o dados de acesso recebidos por e-mail*</span>
 								<span class="space-35"></span>
 								<a href="https://www.eiplus.com.br/entrar/" target="_blank">
 									<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/form.png" alt="Formulário">
@@ -174,7 +182,7 @@
 						<div class="col-sm-7">
 							<h3>Disponibilidade</h3>
 							<p>
-								O acesso gratuito ao aplicativo EI Plus está disponível para clientes de planos residenciais a partir de 30 Mbps
+								O acesso gratuito ao aplicativo Esporte Interativo Plus está disponível para clientes de planos residenciais a partir de 30 Mbps
 							</p>
 						</div>
 
