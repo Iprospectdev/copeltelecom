ba6cc0fda7aeafc2ac4a41eb1c6e4806ff3b59be
Cristian Schütz <cristianschutz@hotmail.com>
2017-12-01 18:06:23 -0200

    page ei ajustes

--- portal-copel-telecom/page-esporte-interativo.php
+++ portal-copel-telecom/page-esporte-interativo.php
@@ -41,7 +41,7 @@
 						<span class="space-50"></span>
 						<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/logo.jpg" class="logo-ei" alt="Canais Esporte Interativo">
 						<h3>A paixão e a emoção pelo esporte, onde você estiver</h3>
-						<p>Assista aos principais conteúdos oferecidos pelos Canais Esporte Interativo e fique por dentro de tudo o que acontece no mundo dos esportes. São programas imperdíveis, como a <strong class="text-orange">UEFA Champions League</strong> (maior competição de clubes do mundo) e a <strong class="text-orange">E-League</strong> (maior competição de Counter Strike) e muito mais.</p>
+						<p>Assista aos principais conteúdos oferecidos pelos Canais Esporte Interativo e fique por dentro de tudo o que acontece no mundo dos esportes. São programas imperdíveis, como a <strong class="text-orange">UEFA Champions League</strong> (maior competição de clubes do mundo) a <strong class="text-orange">E-League</strong> (maior competição de Counter Strike) e muito mais.</p>
 						<p>Você também pode ficar ligado nas transmissões das lutas mais surpreendentes de <strong class="text-orange">MMA</strong> e em toda a emoção do <strong class="text-orange">Pro Wrestling.</strong> É hora de desfrutar por completo do mundo dos esportes através de toda a programação dos Canais do Esporte Interativo.</p>
 
 					</div>
@@ -67,7 +67,7 @@
 
 							<p>O Esporte Interativo é uma das maiores marcas do mundo dedicada à produção e distribuição de conteúdos esportivos em múltiplas plataformas. Reunindo todo o conteúdo do canal, o Esporte Interativo Plus tem o objetivo de estar próximo do público apaixonado por esportes, levando o melhor da temporada, onde ele estiver.</p>
 
-							<p>No aplicativo Esporte Interativo Plus você pode acompanhar todos os conteúdos dos Canais Esporte Interativo no celular, no computador, na smart TV LG, no tablet ou na Apple TV.</p>
+							<p><strong>No aplicativo Esporte Interativo Plus você pode acompanhar todos os conteúdos dos Canais Esporte Interativo no celular, no computador, na smart TV LG, no tablet ou na Apple TV.</strong></p>
 
 							<a href="#como-acessar" class="bt bt-orange">SAIBA COMO ACESSAR</a>
 						</div>
@@ -98,75 +98,78 @@
 
 					<div class="text-center">
 
-						<h3>Clientes Copel Telecom</h3>
+						<h3>Cliente Copel Telecom</h3>
 
 						<div class="col-sm-10 col-sm-offset-1 text-center">
 
 							<p>
-								Confira abaixo como é fácil baixar o aplicativo do <strong class="text-orange">Esporte Interativo Plus</strong> na seu smartphone ou tablet:<br>
-								*O plano do <strong class="text-orange">Esporte Interativo Plus</strong> permite até dois acessos simultâneos.
+								Confira abaixo como é fácil baixar o aplicativo do <strong class="text-orange">Esporte Interativo Plus</strong> no seu smartphone ou tablet.
 							</p>
 
 						</div>
 
 					</div>
 
-					
-					<ul class="row">
-						<li class="col-sm-3 col-xs-6 mrg30B">
-							<div data-mh="group-li">
-								<i>1</i>
-								<span>Os seus dados de acesso (login e senha) foram enviados para o seu e-mail cadastrado na Copel Telecom*</span>
-								<span class="space-25"></span>
-								<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-email.png" alt="Email">
-							</div>
-						</li>
-						<li class="col-sm-3 col-xs-6 mrg30B">
-							<div data-mh="group-li">
-								<i>2</i>
-								<span>Acesse a loja de aplicativos no seu Smartphone ou tablet e baixe o aplicativo <strong>Esporte Interativo Plus</strong></span>
-								<span class="space-20"></span>
-								<a href="https://itunes.apple.com/us/app/esporte-interativo-ao-vivo/id509496846?ls=1&mt=8" target="_blank">
-									<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-store.png" alt="App Store">
-								</a>
-								<a href="https://play.google.com/store/apps/details?id=com.ideastek.esporteinterativo3" target="_blank">
-									<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-play.png" alt="Google Play">
-								</a>
-								<small>Você também pode fazer o seu login pelo computador em <a href="http://www.eiplus.com.br/entrar/" target="_blank">www.eiplus.com.br/entrar/</a></small>
-							</div>
-						</li>
-						<li class="col-sm-3 col-xs-6 mrg30B">
-							<div data-mh="group-li">
-								<i>3</i>
-								<span>No aplicativo <strong>Esporte Interativo Plus</strong>, faça o login com o dados de acesso recebidos por e-mail*</span>
-								<span class="space-35"></span>
-								<a href="https://www.eiplus.com.br/entrar/" target="_blank">
-									<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/form.png" alt="Formulário">
-								</a>
-								<small>* Após o seu primeiro acesso, recomendamos que você altere a sua senha.</small>
-							</div>
-						</li>
-						<li class="col-sm-3 col-xs-6 mrg30B">
-							<div data-mh="group-li">
-								<i>4</i>
-								<span>Aproveite! Assinante Copel Telecom tem até dois acessos simultâneos</span>
-								<span class="space-10"></span>
-								<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-el-plus.png" alt="El Plus">
-							</div>
-						</li>
-					</ul>
+					<div class="row">
+						
+						<div class="col-md-10 col-md-offset-1">
+						
+							<ul class="row">
+								<li class="col-sm-4 col-xs-12 mrg30B">
+									<div data-mh="group-li">
+										<i>1</i>
+										<span><strong>Baixe o aplicativo Esporte Interativo Plus</strong> na Play Store ou na Apple Store.</span>
+										<span class="space-20"></span>
+										<a href="https://play.google.com/store/apps/details?id=com.ideastek.esporteinterativo3" target="_blank">
+											<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-play.png" alt="Google Play">
+										</a>
+										<a href="https://itunes.apple.com/us/app/esporte-interativo-ao-vivo/id509496846?ls=1&mt=8" target="_blank">
+											<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/i-store.png" alt="App Store">
+										</a>
+										<small>Você também pode fazer o seu login pelo computador em <a href="http://www.eiplus.com.br/entrar/" target="_blank">www.eiplus.com.br/entrar/</a></small>
+									</div>
+								</li>
+								<li class="col-sm-4 col-xs-12 mrg30B">
+									<div data-mh="group-li">
+										<i>2</i>
+										<span>No app Esporte Interativo Plus, <strong>faça login com seu e-mail cadastrado na Copel Telecom. A senha é o seu CPF.</strong></span>
+										<span class="space-25"></span>
+										<span class="space-100 hidden-md hidden-lg"></span>
+										<span class="space-50 hidden-md hidden-lg"></span>
+										<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/passo-2.png" class="pos-absolute" alt="">
+									</div>
+								</li>
+								<li class="col-sm-4 col-xs-12 mrg30B">
+									<div data-mh="group-li">
+										<i>3</i>
+										<span>Pronto, aproveite! <strong>Assinante Copel Telecom tem até dois acessos simultâneos.</strong></span>
+										<span class="space-35"></span>
+										<a href="https://www.eiplus.com.br/entrar/" target="_blank">
+											<img src="<?php bloginfo('template_url'); ?>/_assets/images/esporte-interativo/passo-3.png" alt="">
+										</a>
+									</div>
+								</li>
+							</ul>
+
+						</div>
+
+					</div>
 
 				</article>
 
-				<span class="space-25 hidden-sm"></span>
-
-				<small class="text-center display-block">*Caso você não tenha recebido o e-mail, o login é seu e-mail cadastrado na Copel Telecom e sua senha você pode recuperar no site <strong><u>www.eiplus.com.br/entrar</u></strong><br> escolhendo a opção <strong>“Esqueci minha senha”</strong>. Caso ainda não tenha conseguido acessar, entre em contato pelo e-mail <strong>relacionamento@esporteinterativoplus.com.br</strong> </small>
+				<small class="text-center display-block"></strong>Após o seu primeiro acesso, recomendamos que você altere a sua senha.</strong> </small>
 				
-				<span class="space-20"></span>
+				<span class="space-40"></span>
 
-				<small class="text-center display-block">Já se cadastrou mas esqueceu sua senha? Entre no site www.eiplus.com.br, clique na opção <strong>“Esqueci minha senha”</strong> e informe seu e-mail ou telefone cadastrado.</small>
+				<div class="esporte-interativo-sacesso">
+					<h6>Não consegue acesso?</h6>
+					<div class="post-content">
+						<p>Você pode recuperar sua senha no site <a href="http://www.eiplus.com.br/entrar">www.eiplus.com.br/entrar</a> escolhendo a opção <strong>“Esqueci minha senha”</strong>.</p>
+						<p>Caso ainda não tenha conseguido acessar, entre em contato pelo e-mail <strong><a href="mailto:relacionamento@esporteinterativoplus.com.br">relacionamento@esporteinterativoplus.com.br</a></strong> </p>
+					</div>	
+				</div>
 
-				<span class="space-50"></span>	
+				<span class="space-45"></span>
 
 			</div>
 
