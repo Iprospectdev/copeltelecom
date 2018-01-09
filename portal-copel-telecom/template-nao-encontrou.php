ae08fd6a2e50f9099b8ac6aeca7c3f4270a6f67d
Cristian Schütz <cristianschutz@hotmail.com>
2018-01-09 16:38:00 -0200

    ajuste rodapé atendimento

--- portal-copel-telecom/template-nao-encontrou.php
+++ portal-copel-telecom/template-nao-encontrou.php
@@ -1,15 +1,16 @@
-<div class="nao-encontrou wow fadeIn">
+<div class="main-nao-encontrou wow fadeIn">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-3 col-sm-6 col-xs-12" data-mh="as-aside-group">
-				<h1><strong>Não encontrou</strong>o que precisava?</h1>
-			</div>
-			<div class="col-md-2 col-sm-6 col-xs-12" data-mh="as-aside-group">
-				<a href="<?php $p = get_page_by_title("Contato"); echo get_permalink($p->ID); ?>"><i class="i-as-chat"></i>Formulário <strong>de Contato</strong></a>
-			</div>
-			<div class="col-md-3 col-sm-6 col-xs-12" data-mh="as-aside-group">
-				<span><i class="i-as-fone"></i><strong>Ligue Grátis</strong><em>0800 41 41 81</em></span>
-			</div>
+		<div class="text-center">
+			<h1 class="main-nao-encontrou-tt"><strong>Não encontrou</strong>o que precisava?</h1>
+		</div>
+		<div class="text-center">
+			<ul class="main-nao-encontrou-share">
+				<li><a href="https://www.facebook.com/copeltelecom/?fref=ts" target="_blank"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://twitter.com/copeltelecom" target="_blank"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="https://www.linkedin.com/company/copel-telecom" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="https://www.youtube.com/channel/UCWLv_uuedAttlRGCpDhI3fg" target="_blank"><i class="fa fa-youtube"></i></a></li>
+			</ul>
+			<a href="<?php $p = get_page_by_title("Contato"); echo get_permalink($p->ID); ?>" class="main-nao-encontrou-form"><img src="<?php bloginfo('template_url'); ?>/_assets/images/nao-encontrou-form-contato.png" alt=""></a>
 		</div>
 	</div>
 </div>
