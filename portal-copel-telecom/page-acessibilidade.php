1b9bd6a562ec99bf744271cdd454fe3fc23bdc83
Cristian Schütz <cristianschutz@hotmail.com>
2018-03-05 16:05:39 -0300

    ajustes acessibilidade

--- portal-copel-telecom/page-acessibilidade.php
+++ portal-copel-telecom/page-acessibilidade.php
@@ -6,8 +6,11 @@
 			<div class="container">
 				<div id="acessibilidade-intro">
 					<div class="row">
-						<div class="col-sm-8 col-sm-offset-2 col-xs-12">
+						<div class="col-sm-10 col-sm-offset-1 col-xs-12">
+							<p>A Copel é uma empresa conectada a pessoas com necessidades especiais e oferece soluções exclusivas a surdos e cegos.</p>
+
 							<p>Em setembro/2008, o website da Copel foi adequado aos princípios de acessibilidade para portadores de deficiência visual, em consonância com as recomendações de acessibilidade para a construção ou adequação de sites e portais da administração pública na rede mundial de computadores (internet), de acordo com a legislação em vigor (Decreto nº 5296 de 02/12/2004).</p>
+
 							<p>Nosso website possibilita que todas as informações e serviços aqui contidos sejam acessados também por pessoas com deficiências visuais.</p>
 						</div>
 					</div>
@@ -16,27 +19,22 @@
 		</header>
 		<div class="container">
 			<div class="row">
-				<article class="col-sm-6 col-xs-12">
-					<h6><i class="i-a-visao"></i>Software para leitura de tela</h6>
+				<article class="col-xs-12 text-left">
+					<h6>Atendimento em Libras Copel Telecom<i class="i-a-visao mrg10L"></i></h6>
 					<p>
-						O leitor de tela é um programa de computador que possibilita o acesso de pessoas com deficiência visual ao conteúdo de um site na Internet. A Copel recomenda o software livre NonVisual Desktop Access (NVDA) para Windows. 
+						O site da Copel disponibiliza atendimento em LIBRAS, utilizando um tradutor específico para esta língua - a Linguagem Brasileira de Sinais. 
 					</p>
-					<p>
-						O NVDA torna o conteúdo acessível através de um sintetizador de voz e possui uma versão portátil, que pode ser executada sem necessidade de instalação. Para baixar o programa, acesse: <a href="http://www.nvda-project.org/wiki/Download" targer="_blank">http://www.nvda-project.org/wiki/Download</a>.
+					<p class="mrg20L">
+						<strong>• VLibras</strong><br><br>
+						O VLibras é uma ferramenta que traduz conteúdos digitais (como textos, áudio e vídeo) para LIBRAS. Você tem acesso a ele em qualquer computador, smartphone ou tablet.<br>
+						Instale o plugin e siga o tutorial.
 					</p>
-				</article>
-				<article class="col-sm-6 col-xs-12">
-					<h6><i class="i-a-nav"></i>Navegação pelo Teclado</h6>
-					<ol>
-						<li>Para chegar à "Página Inicial" deve ser pressionada a tecla Alt em conjunto com a letra I (Alt + I).</li>
-						<li>Para chegar ao conteúdo específico de uma página devem ser pressionadas as teclas Alt + C.</li>
-						<li>Para chegar ao menu devem ser pressionadas as teclas Alt + M.</li>
-						<li>As observações anteriores referem-se ao navegador Internet Explorer. No caso do Firefox, ao invés de Alt + letra, devem ser pressionadas simultaneamente as teclas Alt + Shift + letra.</li>
-						<li>Ao pressionar as teclas alternativas, o Firefox encaminha direto para a página solicitada. Já o Internet Explorer apenas posiciona o foco no link, que deve ser acionado ao clicar com mouse ou pressionar ENTER com teclado.</li>
-					</ol>
+					<a href="http://www.vlibras.gov.br/" class="bt bt-orange mrg20B" target="_blank">Instalar o VLibras</a>
+					<p>Para usar, você seleciona o assunto e, com apenas um toque, o aplicativo traduz para LIBRAS. </p>
 				</article>
 			</div>
 		</div>
+		<span class="space-30"></span>
 		<aside><?php get_template_part('template','highlight-blog'); ?></aside>
 	</section>
 
