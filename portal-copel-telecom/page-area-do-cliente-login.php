4fc870941f2969c443b00b9a0c741da3bc46076e
Cristian Schütz <cristianschutz@hotmail.com>
2018-09-18 21:53:40 -0300

    ajuste area do cliente e faq

--- portal-copel-telecom/page-area-do-cliente-login.php
+++ portal-copel-telecom/page-area-do-cliente-login.php
@@ -1,27 +1,3 @@
-<?php get_header();?>
-	<section class="area-de-cliente" id="login">
-		<header class="wow fadeIn">
-			<h1 class="main-tt">Área do Cliente</h1>
-			<small>Digite login e senha para acessar<br>a Área do Cliente da Copel Telecom.</small>
-		</header>
-		<div class="container wow fadeIn">
-			<form action="">
-				<div class="row">
-					<div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
-						<div class="row">
-							<div class="col-sm-5 col-xs-12"><label><input type="text" placeholder="Login"></label></div>
-							<div class="col-sm-5 col-xs-12"><label><input type="text" placeholder="Senha"></label><a href="#">Esqueci minha senha</a></div>
-							<div class="col-sm-2 col-xs-12"><button class="bt-o-b bt-o-b-white">ENTRAR</button></div>
-						</div>
-						<p>Lembre-se!<br>O login é o número do CPF do titular da conta da Copel Telecom ou o e-mail cadastrado pelo titular da conta. </p>
-					</div>
-				</div>
-			</form>
-			<ul class="row">
-				<li class="col-md-4 col-sm-12">Consultar <strong>protocolo</strong></li>
-				<li class="col-md-4 col-sm-6"><strong>Sou cliente</strong>Copel Telecom <small>Efetue o login acima para consultar seus protocolos.</small></li>
-				<li class="col-md-4 col-sm-6"><strong>Sou cliente</strong>Copel Telecom <a href="#">Consulte aqui seu protocolo.</a></li>
-			</ul>
-		</div>		
-	</section>
-<?php get_footer(); ?>
+<?php
+	wp_redirect( "https://www.copeltelecom.com/autoatendimento/pub/login.jsf" );
+?>
