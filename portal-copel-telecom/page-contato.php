167abdf53ae8e39a5fba620969ffb8fb9bd6460e
Cristian Schütz <cristianschutz@hotmail.com>
2019-06-07 15:52:25 -0300

    ajuste select de serviços contato

--- portal-copel-telecom/page-contato.php
+++ portal-copel-telecom/page-contato.php
@@ -93,7 +93,8 @@
 								<input type="text" name="motivo-text" readonly required>
 								<input type="hidden" name="motivo" value="">
 								<button type="button" data-toggle="dropdown"><i class="fa fa-angle-down"></i></button>
-								<ul class="dropdown-menu">
+								<ul class="dropdown-menu">								
+									<li><a href="#" data-text="Aquisição de Internet Residencial" data-value="Aquisição de Internet Residencial">Aquisição de Internet Residencial</a></li>
 									<li><a href="#" data-text="Aquisição de Serviços Corporativos" data-value="Aquisição de Serviços Corporativos">Aquisição de Serviços Corporativos</a></li>
 									<li><a href="#" data-text="Informações Produtos" data-value="Informações Produtos">Informações Produtos</a></li>
 									<li><a href="#" data-text="Financeiro" data-value="Financeiro">Financeiro</a></li>
